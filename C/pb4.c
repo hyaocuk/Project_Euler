176dab1a0364ddaf623ed5d4fa4c6f1b4ac91865
Terence Ng <awesome.terence@gmail.com>
2013-06-20 09:48:12 +0800

    Problem 4

--- C/pb4.c
+++ C/pb4.c
@@ -14,39 +14,41 @@ int main() {
 	int a, b, c, d, e, f, g, r, product;
 	for(i=999; i>=100; i--) {
 		for(j=999; j>=100; j--) {
-			product = i*j;
-			if(product<100000) {
-				a = product / 10000;
-				r = product % 10000;
-				b = r / 1000;
-				r = r % 1000;
-				c = r / 100;
-				r = r % 100;
-				d = r / 10;
-				r = r % 10;
-				e = r;
-				//printf("%d %d %d %d %d ", a, b, c, d, e);
-				if(a==e && b==d && product>max)
-				{
-					max = product;
-					break;
+			if(i>=j) {
+				product = i*j;
+				if(product<100000) {
+					a = product / 10000;
+					r = product % 10000;
+					b = r / 1000;
+					r = r % 1000;
+					c = r / 100;
+					r = r % 100;
+					d = r / 10;
+					r = r % 10;
+					e = r;
+					//printf("%d %d %d %d %d ", a, b, c, d, e);
+					if(a==e && b==d && product>max)
+					{
+						max = product;
+						break;
+					}
 				}
-			}
-			else if(product>=100000 && product<998002) {
-				a = product / 100000;
-				r = product % 100000;
-				b = r / 10000;
-				r = r % 10000;
-				c = r / 1000;
-				r = r % 1000;
-				d = r / 100;
-				r = r % 100;
-				e = r / 10;
-				r = r % 10;
-				f = r;
-				if(a==f && b==e && c==d && product>max) {
-					max = product;
-					break;
+				else if(product>=100000 && product<998002) {
+					a = product / 100000;
+					r = product % 100000;
+					b = r / 10000;
+					r = r % 10000;
+					c = r / 1000;
+					r = r % 1000;
+					d = r / 100;
+					r = r % 100;
+					e = r / 10;
+					r = r % 10;
+					f = r;
+					if(a==f && b==e && c==d && product>max) {
+						max = product;
+						break;
+					}
 				}
 			}
 		}
