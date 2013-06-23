ff34cd65456e3392970b0e17bfdedb1681bb9c1b
Terence Ng <awesome.terence@gmail.com>
2013-06-23 18:41:02 +0800

    Problem 7

--- C/pb7.c
+++ C/pb7.c
@@ -8,47 +8,46 @@
 #include<stdio.h>
 #include<math.h>
 
-int prime(double n) {
-	int i, t, sign;
+int isPrime(long n) {
+	int r;
+	int f;
 	if(n==1)
-		return 2;
-	else if(n==2)  //this is important
-		return 3;
+		return 0;
+	else if(n<4)
+		return 1;
+	else if(n%2==0)
+		return 0;
+	else if(n<9)
+		return 1;
+	else if(n%3==0)
+		return 0;
 	else {
-		for(t=2; t<=(int)sqrt(n); t++) {
-			if((int)n%t==0) {
-				sign = 0;
-				break;
-			}
-			else {
-				sign = 1;
-			}
+		r = (int)sqrt((double)n);
+		f = 5;
+		while(f<=r) {
+			if(n%f==0)
+				return 0;
+			else if(n%(f+2)==0)
+				return 0;
+			f += 6;
 		}
-		if(sign==1)
-			return n;
-		else return 0;
+		return 1;
 	}
 }
 
 int main() {
-	double i;
-	i = 1.0;
-	int sum;
-	sum = 0;
-	int result[20000];
-	int j, k;
-	j = 0;
-	while(prime(i)<171110) {
-		if(prime(i)!=0) {
-			result[j] = prime(i);
-			j += 1;
-			if(j==10001)
-				break;
+	int candidate;
+	candidate = 1;
+	int count;
+	count = 1;
+	while(count<10001) {
+		candidate += 2;
+		if(isPrime(candidate)==1) {
+			count += 1;
 		}
-			//printf("%d ", prime(i));
-		sum += prime(i);
-		i += 1.0;
 	}
-	printf("the %d-th's result is:%d \n", j, result[j-1]);
+
+	printf("the result is: %d\n", candidate);
+
 	return 0;
 }
